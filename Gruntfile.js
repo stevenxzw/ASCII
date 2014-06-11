8488a1d6673d39142622ee0ad685a73ad652ca20
stevenxzw <stevenxzw@gmail.com>
2014-06-11 19:06:45 +0800

    test

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,19 +5,72 @@ module.exports = function(grunt) {
 	
 	app : {
 		src : 'public/src-js',
-		dist : 'public/js'
+		dist : 'public/js',
+		temp : 'temp/'
 	},
 	
-	
-    uglify: {
+	uglify : {
       options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+        mangle: true
       },
-      build: {
-        src: '<%= app.src %>.js',
-        dest: '<%= app.dist %>.min.js'
+      main: {
+        options: {
+            banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+        },
+        src: '<%= app.temp %>qq.js',
+        dest: '<%= app.temp %>qq.min.js'
       }
-    },
+	},
+	
+	ascii : {
+		toAsciiCode : {
+			options : {
+				
+			},
+			
+			files : {
+				'<%= app.temp %>/ascii.js' : '<%= app.temp %>qq.min.js'
+			}
+		},
+		
+		
+		toEndCode : {
+			options : {
+				
+			},
+			
+			files : {
+				'<%= app.temp %>/gfq.js' : '<%= app.temp %>/ascii.js'
+			}
+		}		
+	},
+	
+	copy: {
+		v5: {
+			files: [
+				{
+					expand:true,
+					cwd: '<%= app.temp %>/',
+					src: 'gfq.js',
+					dest: '../../workspace/gitwork/gaofen-js/js/',
+					filter: 'isFile'
+				}
+			]
+		},
+		
+		v2: {
+			files: [
+				{
+					expand:true,
+					cwd: '<%= app.temp %>/',
+					src: 'gfq.js',
+					dest: '../../workspace/gitwork/gaofen-static-v2/static/js',
+					filter: 'isFile'
+				}
+			]
+		}
+	},
+	
 	
 	watch: {
 	  gruntfile: {
@@ -26,13 +79,19 @@ module.exports = function(grunt) {
 	}
   });
   // Load the plugin that provides the "uglify" task.
-  var task  = grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('my-contrib-ascii');
   
-  grunt.loadNpmTasks('grunt-contrib-ascii');
-  
-  console.log(task);
+
   
   
   // Default task(s).
+  grunt.registerTask('gfq', ['uglify', 'ascii', 'copy']);
+  
   grunt.registerTask('default', ['uglify']);
+  
+  grunt.registerTask('gfqv5', ['uglify', 'ascii', 'copy:v5']);
+  grunt.registerTask('gfqv2', ['uglify', 'ascii', 'copy:v2']);
+  
 };
